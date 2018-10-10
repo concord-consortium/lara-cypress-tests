ceda869447e85d542e532c1fcd4dad0ad764a9da
Kevin Santos <ksantos@CC-KSANTOS.local>
2018-10-10 10:37:17 -0400

    merging into master

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,6 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", () => {
+  cy.visit('https://learn.staging.concord.org/auth/login')
+  cy.get('#username').type('GGrey');
+  cy.get('#password').type('GGrey1');
+  cy.get('#submit').click();
+})
